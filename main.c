688d338e14543d12e3f3298ee39a914badcc91c2
linetmungai <145919795+linetmungai@users.noreply.github.com>
2023-10-18 12:28:41 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -3,14 +3,20 @@
 
 int main()
 {
-    int a , b , c, d ;
-    printf("enter the first value : ");
-    scanf("%d" , &a );
-    printf("enter the second value : ");
-    scanf("%d" , &b);
-    printf("enter the third value : ");
-    scanf("%d" , &c );
-    d = a + b + c ;
-    printf("%d + %d + %d = %d" , a,b,c,d);
+    char unit;
+    printf("whats the unit of your temperature:");
+    scanf("%s",&unit);
+
+    float temperature;
+    printf("enter your temperature:");
+    scanf("%f",&temperature);
+    printf("conversion from farenheit to celcius\n");
+    while(temperature<100)
+    {
+    float value;
+    value = (temperature *5/9)+32;
+    printf("%.2f degrees in Farenheit = %.2f degrees in celcius\n",temperature , value);
+    temperature= temperature+10;
+    }
     return 0;
 }
